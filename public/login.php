380dc7b33a2451d3c655c569682c8e1d10e55f14
Fausta Pavalkyte <B00165884@mytudublin.ie>
2025-04-02 09:54:41 +0100

    sessions

--- public/login.php
+++ public/login.php
@@ -1,4 +1,7 @@
-<?php require_once('../template/header.php'); ?>
+<?php
+require_once('../template/header.php');
+require_once('config.php');
+?>
 <link rel="stylesheet" type="text/css" href="../css/signin.css">
     <title>Sign in</title>
 </head>
@@ -6,7 +9,7 @@
 
 <body>
 <div class="container">
-    <form action="" method="post" name="Login_Form" class="form-signin">
+    <form action="index.php" method="post" name="Login_Form" class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputUsername" >Username</label>
         <input name="Username" type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
@@ -17,9 +20,20 @@
                 <input type="checkbox" value="remember-me"> Remember me
             </label>
         </div>
-        <button name="Submit" value="Login" class="button" type="submit">Sign in</button>
+        <button name="Submit" value="Login" class="button" type="submit"">Sign in</button>
 
     </form>
 </div>
 </body>
 </html>
+
+<?php
+if(isset($_POST['Submit'])){
+    if($_POST['Username'] == $Username && $_POST['Password'] == $Password){
+        echo 'Success!';
+        $_SESSION['Username'] = $Username;
+    }
+    else
+        echo 'Incorrect username or password!';
+}
+?>
